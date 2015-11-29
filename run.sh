8946fc0ccfa04f41ece732141b4e9dcc9f6e79ee
Ignacio Ocampo <nafiux@gmail.com>
2015-11-29 10:57:38 +0000

    Plotly Integration

--- run.sh
+++ run.sh
@@ -3,6 +3,6 @@ export LD_LIBRARY_PATH=`pwd`/lib
 export R_HOME=`pwd`
 export R_LIBS=`pwd`/site-library
 # Use cases
-#./R
+./R
 # Run a script with parameters
-./R --no-restore --file=examples/redshift.R --args PID tehuixtla.png
+#./R --no-restore --file=examples/redshift.R --args PID tehuixtla.png
