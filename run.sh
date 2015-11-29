47c336361b23ae010bec8c88cd2b6da0e0cc9628
Ignacio Ocampo <nafiux@gmail.com>
2015-11-29 02:48:42 +0000

    Integration with RPostgreSQL and example for RedShift query

--- run.sh
+++ run.sh
@@ -2,4 +2,7 @@
 export LD_LIBRARY_PATH=`pwd`/lib
 export R_HOME=`pwd`
 export R_LIBS=`pwd`/site-library
-./R
+# Use cases
+#./R
+# Run a script with parameters
+./R --no-restore --file=examples/redshift.R --args PID tehuixtla.png
